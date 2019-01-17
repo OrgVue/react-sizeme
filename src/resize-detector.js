bc4e532b1c9a8d9a6964a54d4848309742e8adce
Rodin van der Hart <rodinhart@gmail.com>
2019-01-17 11:29:37 +0000

    Update resize-detector.js

--- src/resize-detector.js
+++ src/resize-detector.js
@@ -5,13 +5,17 @@ const instances = {}
 // Lazily require to not cause bug
 // https://github.com/ctrlplusb/react-sizeme/issues/6
 function resizeDetector(strategy = 'scroll') {
-  if (!instances[strategy]) {
+  /*if (!instances[strategy]) {
     instances[strategy] = createResizeDetector({
       strategy,
     })
   }
 
-  return instances[strategy]
+  return instances[strategy]*/
+  
+  return createResizeDetector({
+    strategy,
+  })
 }
 
 export default resizeDetector
